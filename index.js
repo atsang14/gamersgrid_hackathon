88d2ea6d83eb34f94c225492b475443ec0975d20
A.T <aytsang14@gmail.com>
2019-03-02 17:30:41 -0800

    radius works, now working on location

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(express.static("public"));
 
 
 app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname, "public/index.html"));
+  res.sendFile(path.join(__dirname, "public/index.html"));  
 });
 
 // Start the server
